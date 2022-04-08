bbf69baff058ba591265159f0206f35f9682f384
riverkim <dev.riverkim@gmail.com>
2022-04-08 23:46:56 +0900

    style:eslintrc react/prop-types off -> on

--- .eslintrc.js
+++ .eslintrc.js
@@ -26,8 +26,8 @@ module.exports = {
     // hooks의 의존성배열이 충분하지 않을때 강제로 의존성을 추가하는 규칙을 완화
     'react/jsx-props-no-spreading': ['warn'],
     // props spreading을 허용하지 않는 규칙 해제
-    'react/prop-types': ['off'],
-    // props의 타입체크를 처리하려면 prop-types보단 차라리 typescript를 사용하는게 낫다.
+    'react/prop-types': ['on'],
+    // props의 타입체크
     'no-underscore-dangle': ['off'],
     // camelCase를 따르는게 좋긴 하지만 `_`를 어쩔수 없이 써야하는 상황을 위해(가령 백엔드가 mongoDB)
   },
