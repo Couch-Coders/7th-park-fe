2a62859cadbaf4eab34b6f74ff740079c59eafd7
min2oyo <min2u@outlook.com>
2022-04-09 22:56:05 +0900

    feat: apply auto lat,lng

--- src/components/ParkDetail/GoogleMap.jsx
+++ src/components/ParkDetail/GoogleMap.jsx
@@ -1,24 +1,24 @@
 import React from 'react';
 import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api';
 
-const googleMapConfig = {
-  apikey: process.env.REACT_APP_GOOGLEMAP_API_KEY,
-};
+export default function Googlemap({ lat: latitude, lng: longitude }) {
+  const googleMapConfig = {
+    apikey: process.env.REACT_APP_GOOGLEMAP_API_KEY,
+  };
 
-const containerStyle = {
-  // width: "300px",
-  height: '350px',
-};
+  const containerStyle = {
+    // width: "300px",
+    height: '350px',
+  };
 
-const center = {
-  lat: 37.5501402,
-  lng: 126.9903773,
-};
+  const center = {
+    lat: Number(latitude),
+    lng: Number(longitude),
+  };
 
-export default function Googlemap() {
   return (
     <LoadScript googleMapsApiKey={googleMapConfig.apikey}>
-      <GoogleMap mapContainerStyle={containerStyle} center={center} zoom={10}>
+      <GoogleMap mapContainerStyle={containerStyle} center={center} zoom={13}>
         <Marker position={center} />
       </GoogleMap>
     </LoadScript>
