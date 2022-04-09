ad24c1e0b86f4e60aaefa2a9e7e5aa3cd90c2aff
min2oyo <min2u@outlook.com>
2022-04-09 22:06:07 +0900

    feat: googleMap-api-key -> .env

--- src/components/ParkDetail/GoogleMap.jsx
+++ src/components/ParkDetail/GoogleMap.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api';
 
+const googleMapConfig = {
+  apikey: process.env.REACT_APP_GOOGLEMAP_API_KEY,
+};
+
 const containerStyle = {
   // width: "300px",
   height: '350px',
@@ -13,7 +17,7 @@ const center = {
 
 export default function Googlemap() {
   return (
-    <LoadScript googleMapsApiKey="AIzaSyAU8ecjrTVmwM0FHgt8IJnMCuVMPY8yIRw">
+    <LoadScript googleMapsApiKey={googleMapConfig.apikey}>
       <GoogleMap mapContainerStyle={containerStyle} center={center} zoom={10}>
         <Marker position={center} />
       </GoogleMap>
