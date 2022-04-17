de9ba76b9ecde8c733dfbfc9f0fd134f89c36396
riverkim <dev.riverkim@gmail.com>
2022-04-17 23:00:38 +0900

    feat:add signup

--- src/pages/SignupPage.jsx
+++ src/pages/SignupPage.jsx
@@ -7,7 +7,34 @@ import { style } from '../styles/SignupPage.styles';
 import { AuthContext } from '../contexts/AuthProviders';
 
 export default function RegisterPage() {
+  const [userId, setUserId] = useState('');
+  const [userNickName, setUserNickName] = useState('');
+
   const { currentUser } = useContext(AuthContext);
+
+  const onUserIdHandler = event => {
+    setUserId(event.currentTarget.value);
+  };
+  const onUserNickNameHandler = event => {
+    setUserNickName(event.currentTarget.value);
+  };
+  console.log(userId, 'userId');
+  console.log(userNickName, 'userNickName');
+  const onSubmit = async event => {
+    event.preventDefault();
+    await fetch('/users/me', {
+      method: 'POST',
+      body: JSON.stringify({
+        gId: userId,
+        uNickname: userNickName,
+      }),
+    })
+      .then(response => response.json())
+      .then(response => {
+        alert('가입되셨습니다.');
+      });
+  };
+
   return (
     <div>
       <Header />
@@ -18,15 +45,21 @@ export default function RegisterPage() {
           size="large"
           placeholder="ID"
           prefix={<UserOutlined />}
+          value={userId}
+          onChange={onUserIdHandler}
         />
         <Input
           className="signupInput"
           size="large"
           placeholder="Nickname"
           prefix={<UserOutlined />}
+          value={userNickName}
+          onChange={onUserNickNameHandler}
         />
         <SignupBtn>
-          <Button type="primary">회원가입</Button>
+          <Button type="primary" onSubmit={onSubmit}>
+            회원가입
+          </Button>
         </SignupBtn>
       </SignupContainer>
       <Footer />
